3393dac784267b3b6abd000a7f3bcd17f78e3d2d
JhonikGames <jhonikauanmb@gmail.com>
2024-12-03 18:18:15 -0300

    create database

--- data/database.sql
+++ data/database.sql
@@ -1,9 +1,16 @@
-CREATE DATABASE socialchat;
+CREATE DATABASE `socialchat`;
 
-USE socialchat;
+USE `socialchat`;
 
-CREATE TABLE post(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(50) NOT NULL,
-    comment VARCHAR(200) NOT NULL
+CREATE TABLE `user`(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `post`(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `title` VARCHAR(50) NOT NULL,
+    `comment` VARCHAR(200) NOT NULL,
+    `owner` INT NOT NULL,
+    FOREIGN KEY (`owner`) REFERENCES `user`(`id`)
 );
